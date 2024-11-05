31050706edbd8d6ae0442d0075d3d5aa7513a774
Levan Jgerenaia <devtoday22@gmail.com>
2024-11-05 16:01:15 +0400

    added success message  when message sent

--- pages/component/contact.js
+++ pages/component/contact.js
@@ -1,16 +1,35 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
-import styles from '@/pages/component/Contact.module.css'
-
-
-
+import styles from '@/pages/component/Contact.module.css';
 
 export default function Contact() {
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        setIsSubmitted(true);
+
+        // Hide success message after 8 seconds
+        setTimeout(() => {
+            setIsSubmitted(false);
+        }, 8000);
+    };
+
     return (
         <>
+            <Head>
+                <title>Contact Us</title>
+            </Head>
+            {isSubmitted && (
+                <div className={styles.successMessage}>
+                    Message Sent! <br></br>
+                    tanks for completing the form
+                </div>
+            )}
             <main className={styles.main}>
-
                 <div className={styles.container}>
-                    <form className={styles.contactForm}>
+                    <form className={styles.contactForm} onSubmit={handleSubmit}>
                         <h2>Contact Us</h2>
                         <div className={styles.row}>
                             <div className={styles.formGroup}>
@@ -23,11 +42,11 @@ export default function Contact() {
                             </div>
                         </div>
                         <div className={styles.formGroup}>
-                            <label htmlFor="email" >Email Address <span className={styles.requered}>*</span></label>
+                            <label htmlFor="email">Email Address <span className={styles.requered}>*</span></label>
                             <input className={styles.inputField} placeholder="Enter email" type="email" id="email" name="email" required />
                         </div>
                         <div className={styles.formGroup}>
-                            <label className={styles.query} >Query Type <span className={styles.requered}>*</span></label>
+                            <label className={styles.query}>Query Type <span className={styles.requered}>*</span></label>
                             <div className={styles.radioGroup}>
                                 <label>
                                     <input className={styles.checkbox} type="radio" name="query-type" value="general-enquiry" required /> <span className={styles.queryText}> General Enquiry</span>
@@ -43,20 +62,13 @@ export default function Contact() {
                         </div>
                         <div className={`${styles.formGroup} ${styles.checkboxGroup}`}>
                             <label>
-                                <input className={styles.checkboxText} type="checkbox" name="consent" required /> <span className={styles.boxText}> consent to being contacted by the team</span>  <span className={styles.requered}>*</span>
+                                <input className={styles.checkboxText} type="checkbox" name="consent" required /> <span className={styles.boxText}> consent to being contacted by the team</span> <span className={styles.requered}>*</span>
                             </label>
                         </div>
                         <button type="submit" className={styles.submitButton}>Submit</button>
                     </form>
-                </div >
-
-
-
-
-
-
-            </main >
-
+                </div>
+            </main>
         </>
     );
 }
